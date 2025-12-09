9795e9f1fd2e1b518bb69d26936adf2beb2544fa
TOkotit <105845895+TOkotit@users.noreply.github.com>
2025-12-09 22:20:41 +0500

    ВРАЖИНЫ ПОДЫХАЮТ ПОД ОГНЁМ МОЕЙ СКЕЛЕДРОБИ

--- ULTRAJAM/Assets/Project_Shooter/Scripts/Gameplay/DamageControl.cs
+++ ULTRAJAM/Assets/Project_Shooter/Scripts/Gameplay/DamageControl.cs
@@ -69,6 +69,7 @@ namespace Shooter.Gameplay
                 Damage = 0;
                 IsDead = true;
             }
+            Debug.Log("OnDamaged.Invoke()");
             OnDamaged.Invoke();
             StartCoroutine(Co_HitGlow());
         }
